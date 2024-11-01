9e15f44cf6ed2523caf748dcc18546da11e4448f
JorgeDS <jrivera@piensads.com>
2024-11-01 14:07:17 -0600

    Adding Astro db

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+import db from '@astrojs/db';
+
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), db()],
 });
