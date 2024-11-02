c7896a06ed19f2e2d0db8847476396ec2781161f
JorgeDS <jrivera@piensads.com>
2024-11-01 18:20:13 -0600

    Issue | Correction of configs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,20 @@
 import { defineConfig } from 'astro/config';
+
 import tailwind from '@astrojs/tailwind';
+
+import react from '@astrojs/react';
+
+import sitemap from '@astrojs/sitemap';
+
+import node from '@astrojs/node';
 
 import db from '@astrojs/db';
 
 export default defineConfig({
-  integrations: [tailwind(), db()],
+  integrations: [tailwind(), react(), sitemap(), db()],
+  output: 'server',
+
+  adapter: node({
+    mode: 'standalone'
+  })
 });
