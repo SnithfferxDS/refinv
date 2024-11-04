9d880e0a27ab1448164321d5cc3ef395b3738b05
Nav1 <snithfferx@outlook.com>
2024-11-04 01:40:18 -0600

    Adding constants

---
+++ src/configs/constants.ts
@@ -0,0 +1,4 @@
+export const APP_URL = import.meta.env.APP_URL;
+export const APP_NAME = import.meta.env.APP_NAME;
+export const APP_DESC = import.meta.env.APP_DESCRIPTION;
+export const APP_VERSION = import.meta.env.APP_VERSION;
