b38a41502465d7ea8eaf1ca2a9d5006fe7353378
Nav1 <snithfferx@outlook.com>
2024-11-04 01:39:57 -0600

    Adding configs

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,12 @@
+/// <reference path="../.astro/db-types.d.ts" />
 /// <reference path="../.astro/types.d.ts" />
+interface ImportMetaEnv {
+  APP_URL: string;
+  APP_NAME: string;
+  APP_DESCRIPTION: string;
+  APP_VERSION: string;
+}
+
+interface ImportMeta {
+  env: ImportMetaEnv;
+}
