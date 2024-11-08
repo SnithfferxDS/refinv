ac3e9a8fef3cee96fe82cea8211572fc5f4ff1d2
JorgeDS <jrivera@piensads.com>
2024-11-07 18:09:55 -0600

    Adding constants

---
+++ src/configs/constants.ts
@@ -0,0 +1,6 @@
+export const API_URL = `${import.meta.env.API_URL}/api`;
+export const API_TOKEN = import.meta.env.API_TOKEN;
+export const APP_URL = import.meta.env.APP_URL;
+export const APP_NAME = import.meta.env.APP_NAME;
+export const APP_VERSION = import.meta.env.APP_VERSION;
+export const APP_DESC = import.meta.env.APP_DESCRIPTION;
