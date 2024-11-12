1160e02399f838a4f7f65893df0fc02679879489
JorgeDS <jrivera@piensads.com>
2024-11-11 18:22:41 -0600

    Lista la cotización rápida

--- src/pages/api/quotations/create.ts
+++ src/pages/api/quotations/create.ts
@@ -6,7 +6,8 @@ import {
     Product_Relations,
     db, like, eq
 } from "astro:db";
-import { generateIdentifier, calcWeight } from "../../../utils/functions";
+import { generateIdentifier, calcWeight, calcImportFees } from "../../../utils/functions";
+import type { FastQuotation } from "../../../types/Quotation";
 
 export async function saveData(data: any) {
     const quotation = await db.insert(Quotations).values({
@@ -52,53 +53,110 @@ export async function search(name: string) {
     return product;
 }
 
-export async function saveFastData(data: any) {
-    if (data.product_id) {
-        const product = await db.select().from(Products).where(eq(Products.id, data.product_id));
-        if (product !== undefined && data.product_id !== undefined) {
-            const product = await db.insert(Products).values({
-                name: data.product,
-                handle: data.product,
-                description: data.link,
-                short_description: data.product,
-                tags: data.product,
-                sku: generateIdentifier("sku"),
-                mpn: generateIdentifier("mpn"),
-                upc: generateIdentifier("upc"),
-                ean: generateIdentifier("ean"),
-                weight: data.weight ?? calcWeight(data.width, data.height, data.length, data.measure_unit),
-                width: data.width,
-                height: data.height,
-                length: data.length
+export async function saveFastData(data: FastQuotation) {
+    const product = await productExists(data.product, {
+        handle: typeof (data.product) === "string" ? data.product : '',
+        description: data.link ?? '',
+        short_desc: typeof (data.product) === "string" ? data.product : '',
+        tags: '',
+        width: data.width ?? 0,
+        height: data.height ?? 0,
+        length: data.length ?? 0,
+        measure_unit: data.measure_unit ?? 'mm',
+        weight: data.weight ?? 0,
+        weight_unit: data.weight_unit ?? 'g'
+    });
+    if (product !== undefined) {
+        let quotationPrice = data.price;
+        let quotationQuantity = data.quantity;
+        let quotationTotal = quotationPrice * quotationQuantity;
+        let store = 0,
+            country = 0,
+            shipping = 0;
+        if (data.importType === 2) {
+            if (data.store_id !== undefined) store = data.store_id;
+            if (data.country_id !== undefined) country = data.country_id;
+            if (data.shipping !== undefined) shipping = data.shipping;
+        }
+        const quotationFees = await calcImportFees(data);
+        if (quotationFees.message === "ok") {
+            const quotation = await db.insert(Quotations).values({
+                number: data.number ?? generateIdentifier('quotation'),
+                quotation_date: data.quotation_date ?? new Date(),
+                total: Math.round(quotationTotal),
             });
-            if (product !== undefined) {
-                let quotationPrice = parseFloat(data.price);
-                let quotationShipping = parseFloat(data.shipping);
-                let quotationQuantity = parseInt(data.quantity);
-                let quotationTotal = quotationPrice * quotationQuantity;
-                let quotDiscount = parseFloat(data.discount);
-                const quotation = await db.insert(Quotations).values({
-                    number: generateIdentifier('quotation'),
-                    quotation_date: data.date,
-                    total: quotationTotal.toFixed(2),
-                    taxes: (quotationTotal * 0.13).toFixed(2),
+            if (quotation !== undefined) {
+                const quotDetails = await db.insert(Quotations_Details).values({
+                    quotation_id: quotation.id,
+                    product_id: product.id,
+                    quantity: quotationQuantity,
+                    price_exportation: quotationFees.prices?.exportation ?? 0,
+                    price_gob: quotationFees.prices?.gob ?? 0,
+                    price_pyme: quotationFees.prices?.pyme ?? 0,
+                    price_retail: quotationFees.prices?.retail ?? 0,
+                    price_wholesale: quotationFees.prices?.wholesale ?? 0,
+                    price_wholesale_vip: quotationFees.prices?.wholesale_vip ?? 0,
+                    store_price: Math.round(quotationPrice ?? 0),
+                    shipping: Math.round(shipping ?? 0),
+                    taxes: quotationFees.importFees?.freightTaxes ?? 0,
+                    store_id: store,
+                    country_id: country,
+                    store_link: data.link ?? "",
+                    weight: data.weight ?? 0,
+                    weight_unit: data.weight_unit ?? "g",
+                    width: data.width ?? 0,
+                    height: data.height ?? 0,
+                    length: data.length ?? 0,
+                    measure_unit: data.measure_unit ?? "mm",
+                    quot_type: 2,
+                    delivery_time: quotationFees.delivery_time?.days ?? 0,
+                    warranty_time: data.warranty_time ?? 0,
                 });
-                if (quotation !== undefined) {
-                    const quotDetails = await db.insert(Quotations_Details).values({
-                        quotation_id: quotation.id,
-                        product_id: product.id,
-                        quantity: quotationQuantity,
-                        price: quotationPrice.toFixed(2),
-                        shipping: quotationShipping.toFixed(2),
-                        taxes: (quotationTotal * 0.13).toFixed(2),
-                        discount: quotDiscount,
-                        store_id: data.store,
-                        country_id: data.country,
-                        store_link: data.link,
-                        quot_type: data.type
-                    });
+                if (quotDetails !== undefined) {
+                    const quotationDetails = await db.update(Quotations).values({
+                        quotation_details: quotDetails.id,
+                        taxes: quotationFees.importFees?.freightTaxes ?? 0,
+                    }).where(eq(Quotations.id, quotation.id));
+                    if (quotationDetails !== undefined) {
+                        return quotation;
+                    }
                 }
             }
         }
+    }
+}
+
+export async function productExists(term: number | string, data: {
+    handle: string;
+    description: string;
+    short_desc: string;
+    tags: string;
+    width: number;
+    height: number;
+    length: number;
+    measure_unit: string;
+    weight: number;
+    weight_unit: string;
+}) {
+    if (typeof (term) === "number") {
+        const product = await db.select().from(Products).where(eq(Products.id, term));
+        return product;
+    } else {
+        const product = await db.insert(Products).values({
+            name: term,
+            handle: data.handle,
+            description: data.description,
+            short_description: data.short_desc,
+            tags: data.tags,
+            sku: generateIdentifier("sku"),
+            mpn: generateIdentifier("mpn"),
+            upc: generateIdentifier("upc"),
+            ean: generateIdentifier("ean"),
+            weight: data.weight ?? calcWeight(data.width ?? 1, data.height ?? 1, data.length ?? 1, data.measure_unit ?? "mm"),
+            width: data.width,
+            height: data.height,
+            length: data.length
+        });
+        return product;
     }
 }
--- src/types/Quotation.ts
+++ src/types/Quotation.ts
@@ -1,5 +1,8 @@
 import { Quotations } from "astro:db";
 
+export type Quotations = Quotation[];
+export type Quotation_Details = Quotation_Detail[];
+export type FastQuotations = FastQuotation[];
 export interface Quotation {
     id: number;
     number: string;
@@ -17,9 +20,6 @@ export interface Quotation {
     updated_at: Date;
 }
 
-export type Quotations = Quotation[];
-
-export type Quotation_Details = Quotation_Detail[];
 export interface Quotation_Detail {
     id: number;
     quotation_id: number;
@@ -36,4 +36,63 @@ export interface Quotation_Detail {
     processed: boolean;
     created_at: Date;
     updated_at: Date;
+}
+
+export interface FastQuotation {
+    product: number | string;
+    number: string;
+    price: number;
+    quantity: number;
+    buyer_id?: number;
+    quoter_id?: number;
+    client_id?: number;
+    quotation_date?: Date;
+    category_id?: number;
+
+    weight?: number;
+    link?: string;
+    weight_unit?: string;
+    shipping?: number;
+    store_id?: number;
+    country_id?: number;
+    images?: string[];
+    height?: number;
+    width?: number;
+    length?: number;
+    measure_unit?: string;
+    importType?: number;
+    warranty_time?: number;
+    delivery?: Date;
+}
+
+export interface NormalQuotation {
+    product: number | string;
+    number: string;
+    price: number;
+    quantity: number;
+    buyer_id?: number;
+    quoter_id?: number;
+    client_id?: number;
+    quotation_date?: Date;
+    category_id?: number;
+
+    link?: string;
+    weight?: number;
+    weight_unit?: string;
+    shipping?: number;
+    store_id?: number;
+    country_id?: number;
+    images?: string[];
+    height?: number;
+    width?: number;
+    length?: number;
+    measure_unit?: string;
+    importType?: number;
+    sku?: string;
+    mpn?: string;
+    upc?: string;
+    ean?: string;
+    currier?: string;
+    warranty_time?: number;
+    delivery?: Date;
 }
